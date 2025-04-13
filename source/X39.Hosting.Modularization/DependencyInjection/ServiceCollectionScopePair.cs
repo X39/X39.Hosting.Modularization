d6a316b6696ab37cddff5f0d1491d30b8f565f26
X39 <marco@silipo.de>
2025-04-14 01:06:37 +0200

    Fix async scope disposal

--- source/X39.Hosting.Modularization/DependencyInjection/ServiceCollectionScopePair.cs
+++ source/X39.Hosting.Modularization/DependencyInjection/ServiceCollectionScopePair.cs
@@ -2,16 +2,11 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace X39.Hosting.Modularization.DependencyInjection;
 
-internal class ServiceCollectionScopePair : IServiceScope
+internal sealed class ServiceCollectionScopePair(IServiceCollection serviceCollection, ServiceProvider serviceProvider)
+    : IServiceScope, IAsyncDisposable
 {
-    internal IServiceCollection ServiceCollection { get; }
-    private readonly IServiceScope _serviceScope;
-
-    public ServiceCollectionScopePair(IServiceCollection serviceCollection, ServiceProvider serviceProvider)
-    {
-        ServiceCollection = serviceCollection;
-        _serviceScope     = serviceProvider.CreateScope();
-    }
+    internal IServiceCollection ServiceCollection { get; } = serviceCollection;
+    private readonly IServiceScope _serviceScope = serviceProvider.CreateScope();
 
     public void Dispose()
     {
@@ -19,4 +14,12 @@ internal class ServiceCollectionScopePair : IServiceScope
     }
 
     public IServiceProvider ServiceProvider => _serviceScope.ServiceProvider;
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_serviceScope is IAsyncDisposable asyncDisposable)
+            await asyncDisposable.DisposeAsync();
+        else
+            _serviceScope.Dispose();
+    }
 }
